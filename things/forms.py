a4b0cd216e2cef94516d80c1ee9a1959ac2dc28c
Bruno Miguel Fonseca Cavaca (k22017608) <bruno.fonseca_cavaca@kcl.ac.uk>
2023-12-03 17:23:45 +0000

    EU GOSTO DE MAMAR NOS PEITOS DA CABRITINHA

--- things/forms.py
+++ things/forms.py
@@ -4,9 +4,10 @@ from .models import Thing
 # Create your forms here.
 
 class ThingForm(forms.ModelForm):
-    model = Thing
-    fields = ['name','description','quantity']
-    widgets = {
-        'description': forms.Textarea(),
-        'quantity': forms.NumberInput()
-    }
+    class Meta:
+        model = Thing
+        fields = ['name','description','quantity']
+        widgets = {
+            'description': forms.Textarea(),
+            'quantity': forms.NumberInput()
+            } 
